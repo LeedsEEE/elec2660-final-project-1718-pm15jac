4070ecb928677c61ba399f31f961fa587fefefe7
Joshua Colton [pm15jac] <pm15jac@eentpc253.leeds.ac.uk>
2017-11-14 13:33:25 +0000

    Initial Commit

---
+++ Angry Gravity/GameScene.h
@@ -0,0 +1,13 @@
+//
+//  GameScene.h
+//  Angry Gravity
+//
+//  Created by Joshua Colton [pm15jac] on 14/11/2017.
+//  Copyright © 2017 University of Leeds. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface GameScene : SKScene
+
+@end
---
+++ Angry Gravity/GameViewController.h
@@ -0,0 +1,15 @@
+//
+//  GameViewController.h
+//  Angry Gravity
+//
+//  Created by Joshua Colton [pm15jac] on 14/11/2017.
+//  Copyright © 2017 University of Leeds. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <SpriteKit/SpriteKit.h>
+#import <GameplayKit/GameplayKit.h>
+
+@interface GameViewController : UIViewController
+
+@end
