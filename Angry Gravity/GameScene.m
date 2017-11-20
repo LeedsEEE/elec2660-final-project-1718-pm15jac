9b470f8bf30c9a2fd68a77402bbb1a9aaa8e0c73
Joshua Colton [pm15jac] <pm15jac@eentpc253.leeds.ac.uk>
2017-11-20 19:43:22 +0000

    Added level transition and general house keeping regarding method structure.

--- Angry Gravity/GameScene.m
+++ Angry Gravity/GameScene.m
@@ -9,7 +9,7 @@
 #import "GameScene.h"
 
 
-@implementation GameScene {
+@implementation GameScene { //creating nodes in the scene
     SKShapeNode *_spinnyNode;
     SKLabelNode *_label;
     SKSpriteNode *_rock;
@@ -22,13 +22,55 @@
 - (void)didMoveToView:(SKView *)view {
     // Setup your scene here
     
+    [self initLevelOne]; //initialize and configure the first level
+    
+    
+}
+
+-(bool)coinContact{
+    float Ax=_arrow.position.x;
+    float Ay=_arrow.position.y;
+    float Rx=_coin.position.x;
+    float Ry=_coin.position.y;
+    float r=(_coin.size.width/2)+12;
+    if (powf(powf(Ay-Ry, 2)+powf(Ax-Rx, 2), 0.5)<=r){
+        return 1;
+        
+    }
+    else{
+        return 0;
+    }
+
+    
+}
+
+
+
+-(int)gravityFind{
+    float Ax=_arrow.position.x;
+    float Ay=_arrow.position.y;
+    float Rx=_rock.position.x;
+    float Ry=_rock.position.y;
+    float r=_gravityField.size.width/2;
+    if (powf(powf(Ay-Ry, 2)+powf(Ax-Rx, 2), 0.5)<=r){
+        return 1;
+    
+    }
+    else{
+        return 0;
+    }
+    
+    
+}
+
+-(void)initLevelOne{
     _gravityField = [SKSpriteNode spriteNodeWithImageNamed:@"circle.png"];
     _gravityField.position = CGPointMake(100,100);
     _gravityField.size =CGSizeMake(300, 300);
     /*
-    gravityField.physicsBody = [SKPhysicsBody bodyWithCircleOfRadius:gravityField.size.width/2];
-    gravityField.physicsBody.dynamic = NO;
-    gravityField.physicsBody.collisionBitMask =0b0000;
+     gravityField.physicsBody = [SKPhysicsBody bodyWithCircleOfRadius:gravityField.size.width/2];
+     gravityField.physicsBody.dynamic = NO;
+     gravityField.physicsBody.collisionBitMask =0b0000;
      */
     [self addChild: _gravityField];
     
@@ -38,9 +80,9 @@
     _coin.size =CGSizeMake(20, 20);
     _coin.physicsBody = [SKPhysicsBody bodyWithCircleOfRadius:_coin.size.width/2];
     _coin.physicsBody.dynamic = NO;
-
+    
     [self addChild: _coin];
-
+    
     
     
     
@@ -70,10 +112,10 @@
     _arrow.size =CGSizeMake(50, 10);
     _arrow.zRotation =M_PI;
     //_arrow.physicsBody.velocity = self.physicsBody.velocity;
-
+    
     //arrow.physicsBody.collisionBitMask =0b0011;
     _arrow.physicsBody = [SKPhysicsBody bodyWithRectangleOfSize: CGSizeMake(_arrow.size.width,
-                                                           _arrow.size.height)];
+                                                                            _arrow.size.height)];
     
     _arrow.physicsBody.velocity = CGVectorMake(100, 0);
     _arrow.physicsBody.dynamic = YES;
@@ -81,101 +123,19 @@
     
     [self addChild: _arrow];
     
-    
-    
-    
-    
-    
-    
     [_rock runAction:[SKAction repeatActionForever:[SKAction rotateByAngle:M_PI/14 duration:1]]];
     
-    // Get label node from scene and store it for use later
-    _label = (SKLabelNode *)[self childNodeWithName:@"//helloLabel"];
-    
-    _label.alpha = 0.0;
-    [_label runAction:[SKAction fadeInWithDuration:2.0]];
-    
-    CGFloat w = (self.size.width + self.size.height) * 0.05;
-    
-    // Create shape node to use during mouse interaction
-    _spinnyNode = [SKShapeNode shapeNodeWithRectOfSize:CGSizeMake(w, w) cornerRadius:w * 0.3];
-    _spinnyNode.lineWidth = 2.5;
-    
-    [_spinnyNode runAction:[SKAction repeatActionForever:[SKAction rotateByAngle:M_PI duration:1]]];
-    [_spinnyNode runAction:[SKAction sequence:@[
-                                                [SKAction waitForDuration:0.5],
-                                                [SKAction fadeOutWithDuration:0.5],
-                                                [SKAction removeFromParent],
-  
-                                                ]]];
-}
-
--(bool)coinContact{
-    float Ax=_arrow.position.x;
-    float Ay=_arrow.position.y;
-    float Rx=_coin.position.x;
-    float Ry=_coin.position.y;
-    float r=(_coin.size.width/2)+12;
-    if (powf(powf(Ay-Ry, 2)+powf(Ax-Rx, 2), 0.5)<=r){
-        return 1;
-        
-    }
-    else{
-        return 0;
-    }
-
-    
-   /*
-    NSArray *contact = _arrow.physicsBody.allContactedBodies;
-    NSArray *contactCoin = (0);
-    
-    int length = contact.count;
-    
-    if (length ==1){
-        if ([contact[0] isEqual:@"_coin"]){
-            return YES;
-        }
-        else{
-            return NO;
-        }
-    }
-    else if (length ==2){
-        if ([contact[0] isEqual:@"_coin"] || [contact[1] isEqual:@"_coin"]){
-            return YES;
-        }
-        else{
-            return NO;
-        }
-    }
-    
-    else{
-        return YES;
-    }
-*/
-    
 }
 
 
+- (void)nextLevel
+{
+    //[self runAction: self.buttonPressAnimation];
+    SKTransition *reveal = [SKTransition doorwayWithDuration:3];
+    SKScene *newScene = [[SKScene alloc] initWithSize: CGSizeMake(1024,768)];
+    [self.scene.view presentScene: newScene transition: reveal];
 
--(int)gravityFind{
-    float Ax=_arrow.position.x;
-    float Ay=_arrow.position.y;
-    float Rx=_rock.position.x;
-    float Ry=_rock.position.y;
-    float r=_gravityField.size.width/2;
-    if (powf(powf(Ay-Ry, 2)+powf(Ax-Rx, 2), 0.5)<=r){
-        return 1;
-    
-    }
-    else{
-        return 0;
-    }
-    
-
-    
-    
 }
-
 
 -(void)update:(CFTimeInterval)currentTime {
 
@@ -193,10 +153,14 @@
         _coin.physicsBody.dynamic = YES;
         [_coin runAction:[SKAction fadeOutWithDuration:2.0]];
         [_coin runAction:[SKAction scaleTo:2.0 duration:2.0]];
-        [SKAction waitForDuration: 2];
-        //[NSThread sleepForTimeInterval:1];
-        //_arrow.physicsBody.dynamic =false;
-        [SKView ]
+
+
+        double delayInSeconds = 4.0; //The following 3 lines that create a delay in time were copied from this online source:
+        dispatch_time_t popTime = dispatch_time(DISPATCH_TIME_NOW, (int64_t)(delayInSeconds * NSEC_PER_SEC));
+        dispatch_after(popTime, dispatch_get_main_queue(), ^(void){
+            [self nextLevel];
+        });
+        
         
     }
     else if ([self coinContact] ==NO){
